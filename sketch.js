993440132c0659aa89095b12b6222badeb694df1
hafsid <50413142+hafsid@users.noreply.github.com>
2021-10-03 10:39:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var edges;
 var flappy,flappyImage,flappystop;
-var back,backImage,backBottom,backBottomImage;
+var back,backImage = "images/day_back.png",backBottom,backBottomImage;
 
 var topImage,bottomImage,Ob1,obG1,Ob2,obG2;
 
@@ -16,11 +16,13 @@ var pause,play,pauseImg,playImg;
 
 var fly,die,point;
 
+var replay,replayImg;
+
 function preload(){
     flappyImage = loadAnimation("images/flappy1.png","images/flappy2.png","images/flappy3.png");
     flappystop = loadAnimation("images/flappy_stop.png");
 
-    backImage = loadImage("images/day_back.png");
+    getBackgroundImg();
     backBottomImage = loadImage("images/bottom_back.png");
 
     topImage = loadImage("images/top.png");
@@ -38,47 +40,59 @@ function preload(){
 
     pauseImg = loadImage("images/pause.png");
     playImg = loadImage("images/play.png");
+    restartImg = loadImage("images/restart.png");
 
     fly = loadSound("sounds/wing.mp3");
     point = loadSound("sounds/point.mp3");
     die = loadSound("sounds/die.mp3");
 
+    
+
 
 }
 
 function setup(){
-  createCanvas(300,300)
+  createCanvas(600,600)
 
-  back = createSprite(150,-100);
+  back = createSprite(150,-200);
   back.addImage("up_back",backImage)
-  back.scale = 4.5;
+  back.scale = 8.5;
 
-  backBottom = createSprite(190,350);
+  backBottom = createSprite(290,650);
   backBottom.addImage("down_back",backBottomImage)
-  backBottom.scale = 4;
+  backBottom.scale = 7.5;
 
-  flappy = createSprite(50,100);
+  flappy = createSprite(150,100);
   flappy.addAnimation("flappy",flappyImage)
   flappy.addAnimation("flappystop",flappystop)
-  flappy.scale = 1.5;
+  flappy.scale = 3.5;
 
-  start = createSprite(150,150)
+  start = createSprite(290,350)
   start.addImage("start",startImage)
+  start.scale = 3
 
-  getReady = createSprite(150,100)
+  getReady = createSprite(290,250)
   getReady.addImage("getReady",getReadyImg)
+  getReady.scale = 3
 
-  gameOver = createSprite(150,150)
+  gameOver = createSprite(279,250)
   gameOver.addImage("gameOver",gameOverImg)
   gameOver.visible = false;
-  gameOver.scale = 1.5
+  gameOver.scale = 3
+
+  restart = createSprite(300,350);
+  restart.addImage("restart",restartImg);
+  restart.scale = 0.3;
+  restart.visible = false;
 
 
-  pause = createSprite(260,14)
+  pause = createSprite(500,30)
   pause.addImage("pause",pauseImg);
+  pause.scale = 3
 
-  play = createSprite(280,14)
+  play = createSprite(550,30)
   play.addImage("play",playImg);
+  play.scale = 3
   play.visible = false;
 
   // scoreSprite = createSprite(20,20);
@@ -88,6 +102,9 @@ function setup(){
   obG1 = new Group();
   obG2 = new Group();
   coinG = new Group();
+
+  flappy.debug = false
+  flappy.setCollider("circle",0,0,5)
 }
 
 function draw(){
@@ -126,7 +143,7 @@ function draw(){
     spawnObs2();
     spawncoin();
 
-    if(flappy.isTouching(obG1)||flappy.isTouching(obG2)){
+    if(flappy.isTouching(obG1)||flappy.isTouching(obG2) || flappy.y >700){
       gameState = "end"
       die.play();
     }
@@ -137,7 +154,7 @@ function draw(){
       point.play();
     }
 
-    if(mousePressedOver(pause) ){
+    if(mousePressedOver(pause) && gameState === "play" ){
       gameState = "pause"
     }
 
@@ -157,22 +174,22 @@ function draw(){
     coinG.setVelocityXEach(0)
     flappy.changeAnimation("flappystop",flappystop)
     pause.changeImage("play",playImg);
-    if(mousePressedOver(play)){
+    if(mousePressedOver(play) && gameState === "pause"){
       gameState = "play";
       play.visible = false;
       pause.visible = true;
-
+      obG1.setVelocityXEach(-2)
+      obG2.setVelocityXEach(-2)
+      coinG.setVelocityXEach(-2)
     }
 
     
   }
 
 
-
-
-
   if(gameState === "end"){
     flappy.changeAnimation("flappystop",flappystop)
+    restart.visible = true;
     gameOver.visible = true;
     back.velocityX = 0
     backBottom.velocityX = 0
@@ -181,25 +198,55 @@ function draw(){
     obG2.setVelocityXEach(0)
     coinG.setVelocityXEach(0)
 
-    
+    if(mousePressedOver(restart)){
+      reset();
+    }
+
+    // if(mousePressedOver(replay)){
+    //   console.log("hi")
+    //   resetfunction();
+    // }
   }
 
   
   drawSprites();
   //text(mouseX+","+mouseY,mouseX,mouseY)
-  stroke("#f2e985");
-  //strokeWeight(5);
+  stroke("#f2e985"); //#f2e985
+  strokeWeight(2);
+  textSize(30)
+  //textStyle("BOLDITALIC")
   fill("#f2e985")
-  text("Score: "+score,23,24)
+  
+  text("SCORE: "+score,23,34)
 }
 
+
+function reset(){
+  score = 0;
+  flappy.x = 150
+  flappy.y = 100
+  gameState = "play"
+  flappy.visible=true;
+  obG1.setVelocityXEach(-4);
+  obG2.setVelocityXEach(-4);
+  coinG.setVelocityXEach(-4);
+  gameOver.visible = false
+  restart.visible = false
+  obG1.destroyEach();
+  coinG.destroyEach();
+  obG2.destroyEach()
+  
+
+}
+
+//top obstacle spawning
 function spawnObs1(){
- if(frameCount %60 === 0){
-   Ob1 = createSprite(400,10,300,300);
+ if(frameCount %120 === 0){
+   Ob1 = createSprite(700,10,300,300);
    Ob1.addImage(topImage);
    Ob1.velocityX = -2;
-   Ob1.scale = 1.0;
-   Ob1.y = random(10,30)
+   Ob1.scale = 2.5;
+   Ob1.y = Math.round(random(5,20))
    gameOver.depth = Ob1.depth;
     gameOver.depth+=1
 
@@ -207,31 +254,35 @@ function spawnObs1(){
     pause.depth+=1
     play.depth = Ob1.depth;
     play.depth+=1
+    restart.depth = Ob1.depth;
+    restart.depth+=1
    obG1.add(Ob1);
  }
 }
 function spawnObs2(){
-  if(frameCount %60 === 0){
-    Ob2 = createSprite(400,10,300,300);
+  if(frameCount %120 === 0){
+    Ob2 = createSprite(700,10,300,300);
     Ob2.addImage(bottomImage);
     Ob2.velocityX = -2;
-    Ob2.scale = 1.0;
-    Ob2.y = random(250,300)
+    Ob2.scale = 2.5;
+    Ob2.y = Math.round(random(520,600))
     gameOver.depth = Ob2.depth;
     gameOver.depth+=1
     pause.depth = Ob2.depth;
     pause.depth+=1
     play.depth = Ob2.depth;
     play.depth+=1
+    restart.depth = Ob2.depth;
+    restart.depth+=1
     obG2.add(Ob2);
   }
  }
  function spawncoin(){
   if(frameCount %120 === 0){
-    coin = createSprite(400,150,300,300);
+    coin = createSprite(700,300,300,300);
     coin.addImage(coinImage);
     coin.velocityX = -2;
-    coin.scale = 1.0;
+    coin.scale = 2.5;
     //coin.y = random(100,200)
     gameOver.depth = coin.depth;
     gameOver.depth+=1
@@ -239,6 +290,26 @@ function spawnObs2(){
     pause.depth+=1
     play.depth = coin.depth;
     play.depth+=1
+    restart.depth = coin.depth;
+    restart.depth+=1
     coinG.add(coin);
   }
  }
+
+ async function getBackgroundImg(){
+  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var responseJSON = await response.json();
+
+  var datetime = responseJSON.datetime;
+  var hour = datetime.slice(11,13);
+  
+  if(hour>=06 && hour<=19){
+      bg = "images/day_back.png";
+  }
+  else{
+      bg = "images/night_back.png";
+  }
+
+  backImage = loadImage(bg);
+  console.log(backImage);
+}
